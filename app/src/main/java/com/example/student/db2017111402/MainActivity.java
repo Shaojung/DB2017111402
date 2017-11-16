6c1822562237d35ee59c31cecaff72696f1b7828
Shaojung <shaojung@gmail.com>
2017-11-16 19:01:31 +0800

    ListView

--- app/src/main/java/com/example/student/db2017111402/MainActivity.java
+++ app/src/main/java/com/example/student/db2017111402/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.student.db2017111402;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -23,5 +28,30 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_list_item_1, data);
         lv.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_add)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("請輸入字串");
+            final EditText ed = new EditText(MainActivity.this);
+            builder.setView(ed);
+            builder.setPositiveButton("確定新增", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    data.add(ed.getText().toString());
+                }
+            });
+            builder.show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
